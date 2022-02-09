adcc97dcca5e8ec3603c13adf3e86d3713a0cdba
Vitaliy Sakaly <Vitaliy.Sakaly@vanongo.com>
2022-02-09 21:03:52 +0200

    Commit during recording

--- src/main/java/org/example/VersionDemo.java
+++ src/main/java/org/example/VersionDemo.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class VersionDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello all");
         System.out.println();
         System.out.println("This program is running version " +
                 VersionDemo.class.getPackage().getImplementationVersion());
